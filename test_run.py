f4d170d917067282e846b809dd01051692b3408c
Katarina Pejcinovic <67714883+Katarina-Pejcinovic@users.noreply.github.com>
2024-03-09 16:35:40 -0800

    add confusion matricies

--- test_run.py
+++ test_run.py
@@ -78,23 +78,23 @@ label_folds = [fold_2_labels, fold_3_labels, fold_4_labels, fold_5_labels]
 print("made folds")
 
 data_folds, label_folds = get_np_folds(data_folds, label_folds)
-print("len data", len(data_folds), len(label_folds))
+#print("len data", len(data_folds), len(label_folds))
 
 KM_metrics_list, KM_f2_list, SVM_metrics_list, SVM_f2_list, RF_metrics_list, RF_f2_list  = manual_cross_val(data_folds, label_folds)
-print("KM", '\n', KM_metrics_list, KM_f2_list, )
-print("SVM", '\n', SVM_metrics_list, SVM_f2_list, )
-print("RF", RF_metrics_list, RF_f2_list)
+# print("KM", '\n', KM_metrics_list, KM_f2_list, )
+# print("SVM", '\n', SVM_metrics_list, SVM_f2_list, )
+# print("RF", RF_metrics_list, RF_f2_list)
 
 KM_f2_mean, KM_f2_var = get_mean_variance(KM_f2_list)
 SVM_f2_mean, SVM_f2_var = get_mean_variance(SVM_f2_list)
 RF_f2_mean, RF_f2_var = get_mean_variance(RF_f2_list)
 
-print("K-Means F2 mean : ", KM_f2_mean)
-print("K-Means F2 variance : ", KM_f2_var)
-print("SVM F2 mean : ", SVM_f2_mean)
-print("SVM F2 variance : ", SVM_f2_var)
-print("Random Forest F2 mean : ", RF_f2_mean)
-print("Random Forest F2 variance : ", RF_f2_var)
+# print("K-Means F2 mean : ", KM_f2_mean)
+# print("K-Means F2 variance : ", KM_f2_var)
+# print("SVM F2 mean : ", SVM_f2_mean)
+# print("SVM F2 variance : ", SVM_f2_var)
+# print("Random Forest F2 mean : ", RF_f2_mean)
+# print("Random Forest F2 variance : ", RF_f2_var)
 
 table = format_results_to_table(KM_metrics_list, KM_f2_list, SVM_metrics_list, SVM_f2_list, RF_metrics_list, RF_f2_list)
 print(table)
